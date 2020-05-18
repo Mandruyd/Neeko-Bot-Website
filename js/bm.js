dde3905eb5cd6155dbac31e68159458757345f16
Laurentiu <lomoralaurentiu.1@gmail.com>
2020-05-18 12:43:34 +0300

    Small changes to CSS & bm.js

--- js/bm.js
+++ js/bm.js
@@ -3,5 +3,13 @@ const mob = document.querySelector('#mob');
 const mobMenu = document.querySelector('#mob-menu');
 const mobClose = document.querySelector('#close');
 
-mob.addEventListener('click', () => mobMenu.style.display = 'inline');
-mobClose.addEventListener('click', () => mobMenu.style.display = 'none');
+mob.addEventListener('click', () => {
+    mobMenu.style.display = 'inline';
+    mobMenu.classList.remove('to-left');
+    mobMenu.classList.add("to-right");
+});
+mobClose.addEventListener('click', () => {
+    mobMenu.classList.remove('to-right');
+    mobMenu.classList.add('to-left');
+    setTimeout(()=>{mobMenu.style.display = 'none';},200)
+});
