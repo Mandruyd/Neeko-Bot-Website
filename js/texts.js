99ca4bcc41a8e5bacc1df5bf4c408117c3dc4d7f
alexutzro <59777844+alexutzro@users.noreply.github.com>
2020-05-19 19:03:49 +0300

    (texts.js): added servers number in the strings list

--- js/texts.js
+++ js/texts.js
@@ -1,11 +1,11 @@
-$(function () {
+$.getJSON('https://api.neeko-bot.xyz/servers', (function (numar) {
     new TypeIt("#typingtext", {
         breakLines: false,
-        strings: ["Share your music tastes with your friends.", "Transform any song with  unique audio effects.", "Play Guess The Song and earn points.", "Organize Karaoke nights with your friends or family.", "Neeko is the easiest way to play music in your server."],
+        strings: ["Share your music tastes with your friends.", `Neeko is already on ${numar.servers} servers! 🎉`, "Transform any song with  unique audio effects.", "Play Guess The Song and earn points.", "Organize Karaoke nights with your friends or family.", "Neeko is the easiest way to play music in your server."],
         speed: 93,
         loop: true,
         deletespeed: 1,
         nextStringDelay: 3000,
         waitUntilVisible: true
     });
-});
+}));
