ee53e7b555057c408a15266643f0ca07e24925d8
Kovacs Alex <alex@atm.moe>
2020-05-19 19:32:14 +0300

    feat(text): don't include servers when API endpoint is down

--- js/texts.js
+++ js/texts.js
@@ -1,11 +1,22 @@
-$.getJSON('https://api.neeko-bot.xyz/servers', (function (numar) {
+$.getJSON('https://api.neeko-bot.xyz/servers', response => typeText(response.servers)).catch(o_O => typeText());
+
+function typeText(servers = null) {
+    const strings = ["Share your music tastes with your friends.",];
+    if (servers) strings.push(`Neeko is already on ${servers} servers! 🎉`);
+    strings.push(...[
+        "Transform any song with  unique audio effects.",
+        "Play Guess The Song and earn points.",
+        "Organize Karaoke nights with your friends or family.",
+        "Neeko is the easiest way to play music in your server."
+    ]);
+
     new TypeIt("#typingtext", {
         breakLines: false,
-        strings: ["Share your music tastes with your friends.", `Neeko is already on ${numar.servers} servers! 🎉`, "Transform any song with  unique audio effects.", "Play Guess The Song and earn points.", "Organize Karaoke nights with your friends or family.", "Neeko is the easiest way to play music in your server."],
+        strings,
         speed: 93,
         loop: true,
         deletespeed: 1,
         nextStringDelay: 3000,
         waitUntilVisible: true
     });
-}));
+};
